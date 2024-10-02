52a503c1c581c4e3cd65b85bd865a2eadff105b2
ScuBix1 <bastian.monnin1@gmail.com>
2024-10-02 09:51:52 +0200

    reprise du front

--- app/components/grid.js
+++ app/components/grid.js
@@ -1,33 +1,24 @@
 import { SIZE } from "../settings.js";
 
 export default class Grid {
-  tbody;
-  constructor(tbody) {
-    this.tbody = tbody;
-  }
-
-  render(board) {
-    this.tbody.innerHTML = "";
-    for (let row=0; row<SIZE; row++) {
-      const tr = document.createElement("tr");
-      for (let col=0; col<SIZE; col++) {
-        const td = document.createElement("td");
-        const card = board.cards[row*SIZE+col];
-        if (card.icon) {
-          
-          // Ajout d'une image
-          const icon = document.createElement("span");
-          icon.innerHTML = card.icon || "";
-          icon.classList.add("material-symbols-outlined", "icon");
-          td.addEventListener('click', () => board.clic(row,col));
-          td.append(icon);
-        }
-        td.classList.add(card.style);
-        tr.append(td);
-      }
-
-      // Ajout d'une ligne
-      this.tbody.append(tr);
+    tbody;
+    constructor(tbody) {
+        this.tbody = tbody;
     }
-  }
+
+    render(board) {
+        for (let row=0; row<SIZE; row++) {
+            const tr = document.createElement("tr");
+            for(let col=0; col<SIZE; col++) {
+
+                const td = document.createElement("td");
+                const icon = document.createElement("span");
+                icon.classList.add("material-symbols-outlined");
+                icon.innerHTML=board.icons[row*SIZE+col]||"";
+                td.append(icon);
+                tr.append(td);
+            }
+            this.tbody.append(tr);
+        }
+    }
 }
--- app/main.js
+++ app/main.js
@@ -1,7 +1,8 @@
-import { Grid } from './components/grid.js';
+import Grid from './components/grid.js';
 import { Board } from './models/board.js';
+
 window.addEventListener('load', () => {
-  const grid = new Grid(document.getElementById('grid'));
-  const board = new Board()
-  grid.render(board);
+    const grid = new Grid(document.getElementById("grid"));
+    const board = new Board();
+    grid.render(board);
 });
